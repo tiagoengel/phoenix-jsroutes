58caa015ddb0683400f2402453ed0f4d68589af0
Tiago Engel <tiagohngl@gmail.com>
2016-06-25 13:48:54 -0300

    0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PhoenixJsroutes.Mixfile do
 
   def project do
     [app: :phoenix_jsroutes,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
