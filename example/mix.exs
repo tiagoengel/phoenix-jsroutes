09271016f65aee247dfdb31f96938d24c366b601
Tommy Fisher <tfisher@dropbox.com>
2018-07-23 10:12:12 -0700

    will 1.4 work?

--- example/mix.exs
+++ example/mix.exs
@@ -5,7 +5,7 @@ defmodule JsRoutesExample.MixProject do
     [
       app: :js_routes_example,
       version: "0.1.0",
-      elixir: "~> 1.3",
+      elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers() ++ [:jsroutes],
       start_permanent: Mix.env() == :prod,
