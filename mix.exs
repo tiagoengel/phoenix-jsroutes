da68583ad57014567fe3e032ec7602757c3c6ecd
Tiago Engel <tiagohngl@gmail.com>
2020-07-31 11:19:50 +0100

    [skip-ci] Version 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixJsroutes.Mixfile do
   def project do
     [
       app: :phoenix_jsroutes,
-      version: "1.0.0",
+      version: "2.0.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
